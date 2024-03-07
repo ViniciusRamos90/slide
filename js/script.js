92cc90c7cb6c16fbc6bb12f7a9644eb21fb32d18
Vinicius Ramos <vluizramos@gmail.com>
2024-03-07 17:17:59 -0300

    adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ import { SlideNav } from './slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next'); // add arrow navigation buttons to the slideshow, passing in their
-console.log(slide);
+
+slide.addControl();
