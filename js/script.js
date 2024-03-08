036c1c1a9599e60ba670a927c32218b92f33b5be
Vinicius Ramos <vluizramos@gmail.com>
2024-03-08 10:23:36 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-import { SlideNav } from './slide.js';
+import SlideNav from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next'); // add arrow navigation buttons to the slideshow, passing in their
 
-slide.addControl();
+slide.addControl('.custom-controls');
